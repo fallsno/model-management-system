45e0c413b3761a835de1eb7112ea858ddcf62093
fallsno <167508393+fallsno@users.noreply.github.com>
2026-04-15 10:40:31 +0800

    Update vite.config.js

--- model-management-system/frontend/vite.config.js
+++ model-management-system/frontend/vite.config.js
@@ -1,8 +1,14 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 3000,
     proxy: {
@@ -10,5 +16,7 @@ export default defineConfig({
       '/uploads': 'http://localhost:8000'
     }
   },
-  build: { outDir: 'dist' }
+  build: {
+    outDir: 'dist'
+  }
 })
